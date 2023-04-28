ff083fcc09e26d3a9b55934869407a36cdc38be4
Ricardo Camarena <ricky_camarena@hotmail.com>
2023-04-27 19:55:58 -0600

    feat: Nueva vista Home

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,15 @@ module.exports = {
     colors: {
       'pokeBlue': '#3B4CCA',
       'pokeYellow': '#FFDE00'
+    },
+    fontSize: {
+      'sm': '0.8rem',
+      'base': '1rem',
+      'xl': '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
     }
   },
   plugins: [],
